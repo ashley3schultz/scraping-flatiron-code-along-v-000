e74c9b8d8f2764fea0353958bb035e2a1a085f7d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-13 22:14:17 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,6 +16,8 @@ class Scraper
   
   def make_courses 
     #doc.css(".post").each do |post|
+    
+    #makecourses.each do |post|
     #  course = Course.new
     #  course.title = post.css("h2").text
     #  course.schedule = post.css(".date").text
