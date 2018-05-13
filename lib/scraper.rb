b1b7ecc87cdbee91a947528f42aaebc885e4ad88
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-13 21:49:27 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def get_page 
     #uses nokogiri to get HTML fron the web page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    doc.css(".post").first.css("h2")
+    doc.css(".post").first.css("h2").text
     binding.pry
   end 
   
